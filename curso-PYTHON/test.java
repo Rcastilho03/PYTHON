aba6e68772bed9e358254eeddecc01cc469f6543
Rcastilho03 <rickricardo0303@gmail.com>
2023-08-29 21:57:36 -0300

    test

---
+++ curso-PYTHON/test.java
@@ -0,0 +1,10 @@
+public final class test {
+  private test() {}
+
+  public static void main(String... args) {
+    var nome = 1234-1233;
+    System.out.println(nome);
+    nome = true;
+    System.out.println(nome);
+  }
+}

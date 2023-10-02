0abf6821b8d269691d7cf91d3fbed14450d3ba4a
Rcastilho03 <rickricardo0303@gmail.com>
2023-10-02 20:30:21 -0300

    jogadores

--- curso-PYTHON/15-Tkinter/jogadores.py
+++ curso-PYTHON/15-Tkinter/jogadores.py
@@ -1,5 +1,7 @@
-#Criando um campo de digitação
+# Foi uma honra vacilar com você
+# Criando um campo de digitação
 import tkinter as tk
+from tkinter import messagebox
 
 class Application:
     def __init__(self, master=None):
@@ -9,62 +11,62 @@ class Application:
         self.pady = "28"
         self.padx = "10"
         #conteiners são subdivisões da janela criada
-        self.ct1 = tk.Frame(master,bg="blue")
+        self.ct1 = tk.Frame(master)
         self.ct1["pady"] = self.pady #Espaçamento do conteiner superior
         self.ct1.pack()
 
-        self.ct2 = tk.Frame(master,bg="yellow")
+        self.ct2 = tk.Frame(master)
         self.ct2["pady"] = self.pady #Espaçamento do conteiner superior
         self.ct2.pack()
 
-        self.ct6 = tk.Frame(master,bg="blue")
+        self.ct6 = tk.Frame(master)
         self.ct6["pady"] = self.pady #Espaçamento do conteiner superior
         self.ct6.pack()
 
-        self.ct11 = tk.Frame(master,bg="yellow")
+        self.ct11 = tk.Frame(master)
         self.ct11["pady"] = self.pady #Espaçamento do conteiner superior
         self.ct11.pack()
 
-        self.ct14 = tk.Frame(master,bg="blue")
+        self.ct14 = tk.Frame(master)
         self.ct14["pady"] = self.pady #Espaçamento do conteiner superior
         self.ct14.pack()
 
 #Containers inside ct2
-        self.ct3 = tk.Frame(self.ct2,bg="green")
+        self.ct3 = tk.Frame(self.ct2)
         self.ct3["pady"] = self.padx #Espaçamento do conteiner superior
         self.ct3.pack(side = "left")
 
-        self.ct4 = tk.Frame(self.ct2,bg="red")
+        self.ct4 = tk.Frame(self.ct2)
         self.ct4["pady"] = self.padx #Espaçamento do conteiner superior
         self.ct4.pack(side = "left")
 
-        self.ct5 = tk.Frame(self.ct2,bg="green")
+        self.ct5 = tk.Frame(self.ct2)
         self.ct5["pady"] = self.padx #Espaçamento do conteiner superior
         self.ct5.pack(side = "left")
 
 #Containers inside ct6
-        self.ct7 = tk.Frame(self.ct6,bg="red")
+        self.ct7 = tk.Frame(self.ct6)
         self.ct7["pady"] = self.padx #Espaçamento do conteiner superior
         self.ct7.pack(side = "left")
 
-        self.ct8 = tk.Frame(self.ct6,bg="green")
+        self.ct8 = tk.Frame(self.ct6)
         self.ct8["pady"] = self.padx #Espaçamento do conteiner superior
         self.ct8.pack(side = "left")
 
-        self.ct9 = tk.Frame(self.ct6,bg="red")
+        self.ct9 = tk.Frame(self.ct6)
         self.ct9["pady"] = self.padx #Espaçamento do conteiner superior
         self.ct9.pack(side = "left")
 
-        self.ct10 = tk.Frame(self.ct6,bg="green")
+        self.ct10 = tk.Frame(self.ct6)
         self.ct10["pady"] = self.padx #Espaçamento do conteiner superior
         self.ct10.pack(side = "left")
 
 #Containers inside ct11
-        self.ct12 = tk.Frame(self.ct11,bg="red")
+        self.ct12 = tk.Frame(self.ct11)
         self.ct12["pady"] = self.padx #Espaçamento do conteiner superior
         self.ct12.pack(side = "left")
 
-        self.ct13 = tk.Frame(self.ct11,bg="red")
+        self.ct13 = tk.Frame(self.ct11)
         self.ct13["pady"] = self.padx #Espaçamento do conteiner superior
         self.ct13.pack(side = "left")
 
@@ -121,7 +123,7 @@ class Application:
 
 
 
-        self.btnSair = tk.Button(self.ct14, text="Sair", font=self.fonteBtn, width=10)
+        self.btnSair = tk.Button(self.ct14, text="SAIR", font=self.fonteBtn, width=10)
         self.btnSair["command"] = master.quit
         self.btnSair.pack()
 
@@ -139,18 +141,25 @@ class Application:
 
 
     def busca(self):
-        nome=self.jogadores.get()
-        if nome in curso:
-            indice = curso.index(nome)
-            self.horas["text"] = horasLista[indice]
+        player=self.jogador.get()
+        print(player)
+        if player in nome:
+            indice = nome.index(player)
+            self.clube["text"] = clubes[indice]
+            self.pais["text"] = países[indice]
+            self.salario["text"] = str(salarios[indice])+" Milhões"
+
         else:
-            self.horas["text"] = "Curso não encontrado"
+            self.clube["text"] = "????????"
+            self.pais["text"] = "????????"
+            self.salario["text"] = "????????"
+            messagebox.showwarning(title="Atenção",message="Jogador não encontrado")
 
 
 nome = ["Alex Telles","Alexsandro",	"Alisson",	"Bruno Guimarães",	"Casemiro",	"Daniel Alves",	"Danilo","Danilo","Eder Militão","Ederson","Fabinho","Fred","Gabriel Martinelli",	"Guilherme Arana",	"Lucas Paquetá",	"Marquinhos",	"Neymar",	"Philippe Coutinho",	"Raphinha",	"Richarlison",	"Rodrygo",	"Thiago Silva",	"Vinícius Junior",	"Weverton"]
-clube=["Manchester",	"Juventus",	"Liverpool",	"Newcastle",	"Real Madrid",	"Barcelona",	"Palmeiras",	"Juventus",	"Real Madrid",	"Manchester",	"Liverpool",	"Manchester",	"Arsenal",	"Atlético-MG"	,"Lyon",	"PSG"	,"PSG",	"Aston Villa",	"Leeds United",	"Everton",	"Real Madrid",	"Chelsea",	"Real Madrid",	"Palmeiras"]
-país = ["Inglaterra","Itália","Inglaterra","Inglaterra","Espanha","Espanha","Brasil","Itália","Espanha","Inglaterra","Inglaterra","Inglaterra","Inglaterra","Brasil","França","França","França","Inglaterra","Inglaterra","Inglaterra","Espanha","Inglaterra","Espanha","Brasil"]
-salario= [2.7,	2.4,	5.1,	2.5,	5.1,	0.98,	0.5,	2.2,	4.2,	4.3,	4.1,	3.4,	0.95,	0.45,	4.5	,7.1,	22,	9.5,	4.1,	2.5,	3.5,	2.8,	5.5,	0.3]
+clubes=["Manchester",	"Juventus",	"Liverpool",	"Newcastle",	"Real Madrid",	"Barcelona",	"Palmeiras",	"Juventus",	"Real Madrid",	"Manchester",	"Liverpool",	"Manchester",	"Arsenal",	"Atlético-MG"	,"Lyon",	"PSG"	,"PSG",	"Aston Villa",	"Leeds United",	"Everton",	"Real Madrid",	"Chelsea",	"Real Madrid",	"Palmeiras"]
+países = ["Inglaterra","Itália","Inglaterra","Inglaterra","Espanha","Espanha","Brasil","Itália","Espanha","Inglaterra","Inglaterra","Inglaterra","Inglaterra","Brasil","França","França","França","Inglaterra","Inglaterra","Inglaterra","Espanha","Inglaterra","Espanha","Brasil"]
+salarios= [2.7,	2.4,	5.1,	2.5,	5.1,	0.98,	0.5,	2.2,	4.2,	4.3,	4.1,	3.4,	0.95,	0.45,	4.5	,7.1,	22,	9.5,	4.1,	2.5,	3.5,	2.8,	5.5,	0.3]
 
 
 janela = tk.Tk()

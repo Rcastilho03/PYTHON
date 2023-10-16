615a7a8e6db797a5228d7dc686fc371b0c75e667
Rcastilho03 <rickricardo0303@gmail.com>
2023-10-16 15:55:17 -0300

    fim do curso

--- curso-PYTHON/test.java
+++ curso-PYTHON/test.java
@@ -1,10 +1,29 @@
-public final class test {
-  private test() {}
-
-  public static void main(String... args) {
-    var nome = "rodrigo";
-    System.out.println(nome);
-    //nome = true;
-    System.out.println(nome);
-  }
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+ 
+public class test {
+    public static void main(String[] args)
+    {
+        Dictionary<String, Integer> dict= new Hashtable<>();
+        dict.put("Alice", 25);
+        dict.put("Bob", 30);
+        dict.put("Charlie", 35);
+ 
+        System.out.println(dict.get("Bob")); // 30
+ 
+        int oldValue = dict.put("Charlie", 40);
+        System.out.println(oldValue); // 35
+ 
+        dict.remove("Alice");
+ 
+        System.out.println(dict.size()); // 2
+ 
+        Enumeration<String> k = dict.keys();
+        while (k.hasMoreElements()) {
+            String key = k.nextElement();
+            System.out.println("Key: " + key + ", Value: "
+                               + dict.get(key));
+        }
+    }
 }
